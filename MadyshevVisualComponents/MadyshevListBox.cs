096cc4f32f1dfc6c91e8c86de88f61c2b4bffc24
ktrepfk <ktrepfk@mail.ru>
2022-09-08 13:51:24 +0400

    Добавьте файлы проекта.

---
+++ MadyshevVisualComponents/MadyshevListBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListBox;
+
+namespace MadyshevVisualComponents
+{
+    public partial class MadyshevListBox : UserControl
+    {
+        public event EventHandler SelectedIndexChanged;
+        public string ListBoxValue
+        {
+            set
+            {
+                if (listBox.Items.Contains(value))
+                {
+                    listBox.Text = value;
+                }
+            }
+            get
+            {
+                return listBox.Text;
+            }
+        }
+        public MadyshevListBox()
+        {
+            InitializeComponent();
+            listBox.SelectedIndexChanged += new System.EventHandler(this.SelectIndexChanged);
+        }
+        public void AddItem(string item)
+        {
+            listBox.Items.Add(item);
+        }
+        public void ClearItems()
+        {
+            listBox.Items.Clear();
+            listBox.ResetText();
+        }
+        private void SelectIndexChanged(object sender, EventArgs e)
+        {
+            Refresh();
+            base.OnSizeChanged(e);
+            SelectedIndexChanged?.Invoke(sender, e);
+        }
+    }
+}
---
+++ MadyshevVisualComponents/MadyshevTextBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MadyshevVisualComponents
+{
+    public partial class MadyshevTextBox : UserControl
+    {
+        public event EventHandler CheckedChanged;
+        public double? TextBoxValue
+        {
+            get
+            {
+                if (checkBox.Checked)
+                {
+                    return null;
+                }
+                if (textBox.Text == "")
+                {
+                    throw new Exception("value not entered");
+                }
+                if (!Regex.IsMatch(textBox.Text, @"((\d+)(\,+)(\d+))$"))
+                {
+                    throw new Exception("invalid format");
+                }
+                return Convert.ToDouble(textBox.Text);
+            }
+            set
+            {
+                if (!checkBox.Checked)
+                {
+                    textBox.Text = value.ToString();
+                }
+            }
+        }
+        public MadyshevTextBox()
+        {
+            InitializeComponent();
+        }
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox.Enabled = !checkBox.Checked;
+            CheckedChanged?.Invoke(sender, e);
+        }
+    }
+}
